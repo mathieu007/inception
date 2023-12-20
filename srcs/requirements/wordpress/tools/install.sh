407f787b70701839fb16660ba7da59336326b58b
Mathieu Roy <mathieuroy1983@gmail.com>
2023-12-20 11:34:44 -0500

    fina push

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -1,3 +1,5 @@
+set -x
+
 if [ -f /.env ]; then
   export $(cat /.env | xargs)
 fi
@@ -6,8 +8,8 @@ cd /var/www/wordpress
 
 # Set up WordPress configuration
 # wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
-# echo "wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PWD --admin_email=$WP_EMAIL --allow-root" > worpress_install_log.txt
-# echo "wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root" >> worpress_install_log.txt
+echo "wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PWD --admin_email=$WP_EMAIL --allow-root" > worpress_install_log.txt
+echo "wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root" >> worpress_install_log.txt
 
 wp core install \
   --url="$DOMAIN_NAME" \
